c3432eb57a5dca4d6470f875e546e460bd3954d5
Brendan <46061381+AtlasDisease@users.noreply.github.com>
2023-11-08 11:01:36 -0600

    Update to variable name
    Description is now epitaph which is the more accurate word

--- districts/places/cemeteries.py
+++ districts/places/cemeteries.py
@@ -20,15 +20,15 @@ class Grave:
     date_born: dt.date = dt.date.min
     date_died: dt.date = dt.date.max
     _: KW_ONLY
-    description: str = field(default = "")
+    epitaph: str = field(default = "")
 
     def __str__(self) -> str:
         return self.name
 
     def __format__(self, format_spec = "") -> str:
         if "F" in format_spec or "O" in format_spec:
-            if self.description:
-                return f"{self.name}\r\n{self.date_born:%b. %d, %Y} - {self.date_died:%b. %d, %Y}\r\n{self.description}"
+            if self.epitaph:
+                return f"{self.name}\r\n{self.date_born:%b. %d, %Y} - {self.date_died:%b. %d, %Y}\r\n{self.epitaph}"
             return f"{self.name}\r\n{self.date_born:%b. %d, %Y} - {self.date_died:%b. %d, %Y}"
 
         return str(self)
