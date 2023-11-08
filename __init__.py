dc882b99162957fa23ee841dce050c794cd26596
Brendan <46061381+AtlasDisease@users.noreply.github.com>
2023-11-08 15:47:26 -0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,5 @@
-# Force you to import cities.py manually as
-# it contains multiple classes and things you need
-
-from .cities import City
-from .counties import County, Parish, Shire
-from .states import State
-from .countries import Country
-from .continents import Continent
-from .planets import Planet
-from .planetarysystems import PlanetarySystem
-from .galaxies import Galaxy
-from .universes import Universe
+from .places import *
+from .stadiums import *
+from .religious import *
+from .cemeteries import *
+from .emergency import *
