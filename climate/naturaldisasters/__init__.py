d08f7eb865bd7cb97efa0a1ed74dbd17d5e1525c
Brendan <46061381+AtlasDisease@users.noreply.github.com>
2024-02-28 15:39:51 -0600

    Update
    - Added naturaldisasters package in climate package

---
+++ climate/naturaldisasters/__init__.py
@@ -0,0 +1,3 @@
+# --- Imports --- #
+
+from .wildfires import Wildfire
---
+++ climate/naturaldisasters/wildfires.py
@@ -0,0 +1,46 @@
+# --- Imports --- #
+
+from dataclasses import dataclass, field
+from typing import Iterable
+
+
+# --- Wildfire Class --- #
+
+@dataclass
+class Wildfire:
+    """A wildfire. size should be in acres, but this is not enforced currently."""
+    name: str
+    size: float = field(default=0)
+    locations: list = field(default_factory=list)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __format__(self, format_spec = "") -> str:
+        if any((char in {"F", "O", "L", "l"} for char in format_spec)):
+            return f"{self.name} {self.__class__.__name__}"
+
+        return str(self)
+
+    def __int__(self) -> int:
+        return int(self.size)
+
+    def __float__(self) -> float:
+        return self.size
+
+    def __iter__(self) -> Iterable:
+        return iter(self.locations)
+
+    def add_location(self, location):
+        self.locations.append(location)
+
+
+if __name__ == "__main__":
+    def main():
+        wildfire = Wildfire("Smokehouse Creek", 850_000)
+        print(f"{wildfire: O}")
+        print(f"{wildfire.size:,d}")
+        print(f"{wildfire.size:,.2f}")
+        print(list(iter(wildfire)))
+    
+    main()
