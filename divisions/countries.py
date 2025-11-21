39a75b2cc8d71c3715b125f85b49a004cebc38db
Brendan <46061381+AtlasDisease@users.noreply.github.com>
2025-11-21 11:18:08 -0600

    Update countries.py

--- divisions/countries.py
+++ divisions/countries.py
@@ -4,7 +4,7 @@
 
 # --- Imports --- #
 
-from typing import override
+from typing import override, Self
 from .divisions import Division
 from .cities import City, AdministrativeTypes
 
@@ -29,7 +29,7 @@ class Country(Division):
     def __init__(self, name: str,
                  /,
                  subdivisions: list[Division] | Division = None,
-                 capitals: list[City] = None,
+##                 capitals: list[City] = None,
                  *,
                  population: int = None,
                  prefix: str = "",
@@ -40,15 +40,16 @@ class Country(Division):
         if prefix:
             self.prefix = prefix
         
-        if capitals:
-            if any((not hasattr(city, "admin_type") for city in capitals)):
-                raise NonCityError("Capitals should have an admin type attribute.")
-            if any((AdministrativeTypes.CAPITAL not in city._admin_type for city in capitals)):
-                raise NonCapitalError("Capitals should have the admin type of CAPITAL.")
-        else:
-            capitals = list(self._find_capitals())
-
-        self._capitals = capitals 
+##        if capitals:
+##            if any((not hasattr(city, "admin_type") for city in capitals)):
+##                raise NonCityError("Capitals should have an admin type attribute.")
+##            if any((AdministrativeTypes.CAPITAL not in city._admin_type for city in capitals)):
+##                raise NonCapitalError("Capitals should have the admin type of CAPITAL.")
+##        else:
+##            capitals = list(self._find_capitals())
+##
+##        self._capitals = capitals
+        self._capitals = list(self._find_capitals())
 
     @override
     def __format__(self, format_spec = ""):
@@ -67,7 +68,7 @@ class Country(Division):
         return self._capitals
 
     @capitals.setter
-    def capitals(self, new_capital):
+    def capitals(self, new_capital: Self):
         #This needs some work to determine whether it should turn into a
         #county seat or a regular city
         self._capitals[0].admin_type, new_capital.admin_type = \
@@ -101,7 +102,6 @@ class Country(Division):
 
         def recurse(division: Division):
             for city in division:
-##                print(city)
                 if not hasattr(city, "admin_type"):
                     recurse(city)
                     continue
